ff0d354493829a5daf5aeef4608ed601161dfbc2
subratsir <business.ttrc@gmail.com>
2023-03-19 08:10:27 +0530

    data binding

--- app/src/main/java/com/example/sharedpreferencesexample/MainActivity.kt
+++ app/src/main/java/com/example/sharedpreferencesexample/MainActivity.kt
@@ -7,28 +7,37 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.sharedpreferencesexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        val text1 = findViewById<TextView>(R.id.text1)
-        val text2 = findViewById<TextView>(R.id.text2)
-        val edit1 = findViewById<EditText>(R.id.edit1)
-        val btn1 = findViewById<Button>(R.id.btn1)
-
+//        val text1 = findViewById<TextView>(R.id.text1)
+//        val text2 = findViewById<TextView>(R.id.text2)
+//        val edit1 = findViewById<EditText>(R.id.edit1)
+//        val btn1 = findViewById<Button>(R.id.btn1)
         val s = GetTextFromSharedPreference("logedinuser", "displayname")
         if (s != null && s.length > 0) {
-            text2.setText(text2.text.toString()+" "+s)
+            binding.text2.setText(binding.text2.text.toString() + " " + s)
         }
-        btn1.setOnClickListener(){
+        binding.btn1.setOnClickListener() {
 
 
-            if(SaveTextToSharedPreference("logedinuser", "displayname", edit1.text.toString())){
-                Toast.makeText(this,"Stored Successfully", Toast.LENGTH_LONG).show()
-            }else{
-                Toast.makeText(this,"Unable to Store! Try later.", Toast.LENGTH_LONG).show()
+            if (SaveTextToSharedPreference(
+                    "logedinuser",
+                    "displayname",
+                    binding.edit1.text.toString()
+                )
+            ) {
+                Toast.makeText(this, "Stored Successfully", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Unable to Store! Try later.", Toast.LENGTH_LONG).show()
             }
         }
     }
@@ -38,7 +47,7 @@ class MainActivity : AppCompatActivity() {
         val sharedPreference: SharedPreferences = getSharedPreferences(name, MODE_PRIVATE)
 
         // get Value
-        val s1: String? = sharedPreference.getString(key,"")
+        val s1: String? = sharedPreference.getString(key, "")
         return s1
     }
 
@@ -50,7 +59,7 @@ class MainActivity : AppCompatActivity() {
         val editor: SharedPreferences.Editor = sharedPreference.edit()
 
         // Save data
-        editor.putString(key,value)
+        editor.putString(key, value)
         return editor.commit()
     }
 
